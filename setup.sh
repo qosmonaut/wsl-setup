8de4eb154d0a35ce9058ba6ea5bc297a0ecb025e
qosmonaut <alenroady@gmail.com>
2024-04-26 19:12:55 +0200

    Fix directory issue

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
-sudo apt-get update -y && sudo apt-get upgrade -y
+#!/usr/bin/env bash
+
+# Get the directory of the script
+DIR="$( dirname -- "$0"; )"
 
 # Set aliases
-cp ./aliases ~/.aliases
+cp DIR/aliases ~/.aliases
 
 # tmux customizations
-cp ./tmux.conf ~/.tmux.conf
+cp DIR/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
+
+# Update and upgrade packages
+sudo apt-get update -y && sudo apt-get upgrade -y
