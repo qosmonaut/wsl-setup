bac90ffd4afc1a51c368037fac5e04b8d1f6516c
qosmonaut <alenroady@gmail.com>
2024-04-26 19:31:06 +0200

    More robust path solution

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Get the directory of the script
-DIR="$( dirname -- "$0"; )"
+DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Set aliases
-cp DIR/aliases ~/.aliases
+cp $DIR/bash_aliases ~/.bash_aliases
 
 # tmux customizations
-cp DIR/tmux.conf ~/.tmux.conf
+cp $DIR/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
 
