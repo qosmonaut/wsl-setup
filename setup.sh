a75884400d995b06007677eca2363b7dc7af3ac7
qosmonaut <alenroady@gmail.com>
2024-04-29 14:18:30 +0200

    Add status output

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Set aliases
+echo "Copying aliases..."
 cp $DIR/bash_aliases ~/.bash_aliases
 
 # tmux customizations
+echo "Setting up tmux..."
 cp $DIR/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
 
 # Update and upgrade packages
+echo "Updating and upgrading packages..."
 sudo apt-get update -y && sudo apt-get upgrade -y
