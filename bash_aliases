94d0feab6f63d843e2d047a5a9fc62967edc3a43
Alen Roady <alenroady@gmail.com>
2024-06-11 11:28:50 +0200

    Use Windows or Linux git depending on filesystem for improved perfomance

--- bash_aliases
+++ bash_aliases
@@ -1,6 +1,7 @@
 # Add WSL integrations
+alias sudo='sudo '
+alias which='which '
 alias open='explorer.exe'
 alias xdg-open='explorer.exe'
 alias copy='clip.exe'
 alias xcopy='clip.exe'
-alias git='git.exe'
--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "Copying aliases..."
 cp $DIR/bash_aliases ~/.bash_aliases
 
+# Use git depending on filesystem
+echo "Copying git script..."
+sudo cp $DIR/git /usr/local/bin/git
+sudo chown root:root /usr/local/bin/git
+
 # Set gitconfig
 echo "Copying git config..."
 cp $DIR/gitconfig ~/.gitconfig
